58c38895a0b4e742f9401c6b2155e1289e2c2f21
kiley <kc17695p@pace.edu>
2025-05-08 19:33:39 -0400

    Briana blackjack fixes

--- Blackjack.java
+++ Blackjack.java
@@ -14,10 +14,12 @@ public class Blackjack {
             return money;
         }
 
-        int playerCard1 = random.nextInt(10) + 2;
-        int playerCard2 = random.nextInt(10) + 2;
-        int dealerCard1 = random.nextInt(10) + 2;
-        int dealerCard2 = random.nextInt(10) + 2;
+        // Function to draw a card and correctly handle face values
+
+        int playerCard1 = getCard(random);
+        int playerCard2 = getCard(random);
+        int dealerCard1 = getCard(random);
+        int dealerCard2 = getCard(random);
 
         int playerTotal = playerCard1 + playerCard2;
         int dealerTotal = dealerCard1 + dealerCard2;
@@ -25,12 +27,11 @@ public class Blackjack {
         System.out.println("Your cards: " + playerCard1 + " and " + playerCard2 + " (Total: " + playerTotal + ")");
         System.out.println("Dealer's visible card: " + dealerCard1);
 
-       
         while (playerTotal < 21) {
             System.out.print("Do you want to hit or stand? (h/s): ");
             char choice = scanner.next().charAt(0);
             if (choice == 'h') {
-                int newCard = random.nextInt(10) + 2;
+                int newCard = getCard(random);
                 playerTotal += newCard;
                 System.out.println("You drew: " + newCard + " (Total: " + playerTotal + ")");
             } else {
@@ -39,13 +40,12 @@ public class Blackjack {
         }
 
         while (dealerTotal < 17) {
-            int newCard = random.nextInt(10) + 2;
+            int newCard = getCard(random);
             dealerTotal += newCard;
         }
 
         System.out.println("Dealer's total: " + dealerTotal);
 
-       
         if (playerTotal > 21) {
             System.out.println("You bust! You lost $" + bet);
             money -= bet;
@@ -59,5 +59,10 @@ public class Blackjack {
 
         System.out.println("Your new balance: $" + money);
         return money;
+    }
+
+    public static int getCard(Random random) {
+        int card = random.nextInt(13) + 1; // Generates values 1-13 (Ace-King)
+        return card > 10 ? 10 : card; // Face cards (J, Q, K) count as 10
     }
 }
